6f12508dcaa0825f31a528bc2a1eb3081fc2f248
achref_limem <achreflimam00@gmail.com>
2025-04-08 00:25:24 +0100

    Medecin Push

---
+++ GestionMedecin/src/main/java/com/example/gestionmedecin/controllers/MedecinController.java
@@ -0,0 +1,51 @@
+package com.example.gestionmedecin.controllers;
+
+import com.example.gestionmedecin.entities.Medecin;
+import com.example.gestionmedecin.services.IMedecin;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/medecin")
+
+
+public class MedecinController {
+    IMedecin iMedecin;
+
+    @GetMapping("/getMedecins")
+    public List<Medecin> getMedecins() {
+        return iMedecin.getMedecins();
+    }
+
+    @GetMapping("/getMedecinByNameAndLastNameAndSpecialization")
+    public Medecin getMedecinByNameAndLastNameAndSpecialization(@RequestParam("first_name") String medecinName, @RequestParam("last_name") String lastName, @RequestParam("specialization") String specialization) {
+        return iMedecin.getMedecinByNameAndLastNameAndSpecialization(medecinName, lastName, specialization);
+    }
+
+
+    @PostMapping("/addMedecin")
+    public Medecin addMedecin(@RequestBody Medecin medecin) {
+        return iMedecin.addMedecin(medecin);
+    }
+
+    @PutMapping("/updateMedecin")
+    public Medecin updateMedecin(@RequestBody Medecin medecin) {
+        return iMedecin.updateMedecin(medecin);
+    }
+    @DeleteMapping("/deleteMedecin/{medecinId}")
+    public void deleteMedecin(@PathVariable("medecinId")  Integer medecinId) {
+        iMedecin.deleteMedecinbyId(medecinId);
+    }
+
+    @DeleteMapping("/deleteMedecinByNameAndLastName")
+    public void deleteMedecinByNameAndLastName(@RequestParam("medecin_Name") String medecinName,@RequestParam("last_Name") String lastName) {
+        iMedecin.deleteMedecinByNameAndLastName(medecinName, lastName);
+    }
+
+
+}
