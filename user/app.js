a6495e51f6f4ec26ab1ef6430784881b1859f1ca
jawher <touil.jawher@esprit.tn>
2025-04-20 12:59:00 +0100

    user project detected in eureka

--- user/app.js
+++ user/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectDB, PORT } = require('./config/db');
+const { registerWithEureka, sendHeartbeat } = require('./config/eureka');
 
 const app = express();
 app.use(express.json());
@@ -7,9 +8,13 @@ app.use(express.json());
 // Connect to MongoDB
 connectDB();
 
+// Register with Eureka
+registerWithEureka();
+setInterval(sendHeartbeat, 30 * 1000); // Send heartbeat every 30 seconds
+
 // Mount routes
 app.use('/api/users', require('./user/userRoutes'));
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 User service running at http://localhost:${PORT}`);
 });
