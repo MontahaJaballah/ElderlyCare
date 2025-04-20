08f38e4f946443e0e24503bd3356a1d058d1d5b3
jawher <touil.jawher@esprit.tn>
2025-04-20 14:30:37 +0100

    spring and front are connected for equipment

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EquipmentListComponent } from './components/equipment-list/equipment-list.component';
+import { EquipmentManagementComponent } from './components/equipment-management/equipment-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/equipment', pathMatch: 'full' },
+  { path: 'equipment', component: EquipmentManagementComponent },
+  { path: 'list', component: EquipmentListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
